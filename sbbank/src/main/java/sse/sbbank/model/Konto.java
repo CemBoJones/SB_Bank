3e7f190a67e0e57c64224bade2928280e75b3604
Marco <stopp.marco@gmail.com>
2015-12-08 16:43:44 +0100

    change package declaration

--- sbbank/src/main/java/sse/sbbank/model/Konto.java
+++ sbbank/src/main/java/sse/sbbank/model/Konto.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sse.sbbank;
+package sse.sbbank.model;
 
 import java.io.Serializable;
 
