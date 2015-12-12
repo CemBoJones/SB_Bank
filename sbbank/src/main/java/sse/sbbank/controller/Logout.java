549e8bef50de015988fe56b14c79af98afb11afa
Marco <stopp.marco@gmail.com>
2015-12-12 17:41:02 +0100

    add class Logout

---
+++ sbbank/src/main/java/sse/sbbank/controller/Logout.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sse.sbbank.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import sse.sbbank.model.User;
+
+/**
+ *
+ * @author Marco
+ */
+@ManagedBean
+@SessionScoped
+public class Logout {
+
+    private User current;
+
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "/home.xhtml?faces-redirect=true";
+    }
+    
+}
