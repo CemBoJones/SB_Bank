bb3765e73fe6b331a9f3f7bfdb9ba294ab2dacf4
Marco <stopp.marco@gmail.com>
2015-12-12 17:15:44 +0100

    add class deleteUserBean

--- sbbank/src/main/java/sse/sbbank/controller/DeleteUserBean.java
+++ sbbank/src/main/java/sse/sbbank/controller/DeleteUserBean.java
@@ -5,10 +5,25 @@
  */
 package sse.sbbank.controller;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import sse.sbbank.controller.dbaccess.DBAccess;
+
 /**
  *
  * @author Marco
  */
+@ManagedBean(name = "deleteUserBean", eager = true)
+@SessionScoped
 public class DeleteUserBean {
+    private int Kontonummer;
+    DBAccess dBAccess;
     
+    public DeleteUserBean() {
+        this.dBAccess = new DBAccess();
+    }
+    
+    public void deleteUser(){
+        dBAccess.deletePersonFromDB(Kontonummer);
+    }
 }
