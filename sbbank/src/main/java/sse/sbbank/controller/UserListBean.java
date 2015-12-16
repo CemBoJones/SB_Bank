205d80843c825d8ec9105aa4731393cf81827986
Marco <stopp.marco@gmail.com>
2015-12-16 11:12:03 +0100

    some fixes on delet and add user functionality

--- sbbank/src/main/java/sse/sbbank/controller/UserListBean.java
+++ sbbank/src/main/java/sse/sbbank/controller/UserListBean.java
@@ -18,12 +18,18 @@ import sse.sbbank.model.User;
 @ManagedBean
 @SessionScoped
 public class UserListBean {
-    private DBAccess dBAccess;
+
+    private DBAccess dbAccess;
     private List<User> userList;
-    
-    public UserListBean(){
-        this.dBAccess=new DBAccess();
-        this.userList=dBAccess.getUserListFromDB();
+    private int kontonummer;
+    private String vorname;
+    private String nachname;
+    private String username;
+    private String passwort;
+
+    public UserListBean() {
+        this.dbAccess = new DBAccess();
+        this.userList = dbAccess.getUserListFromDB();
     }
 
     public List<User> getUserlist() {
@@ -33,5 +39,67 @@ public class UserListBean {
     public void setUserlist(List<User> userlist) {
         this.userList = userlist;
     }
+
+    public void deleteUser() {
+        dbAccess.deletePersonFromDB(kontonummer);
+        dbAccess = new DBAccess();
+        this.userList = dbAccess.getUserListFromDB();
+    }
+
+    public int getKontonummer() {
+        return kontonummer;
+    }
+
+    public void setKontonummer(int kontonummer) {
+        this.kontonummer = kontonummer;
+    }
     
+    public void addNewUser() {
+        User tempUser = new User(nextKontonummer(), vorname, nachname, username, passwort, 0.0, false);
+        dbAccess.insertPersonenToDB(tempUser);
+        dbAccess = new DBAccess();
+        this.userList = dbAccess.getUserListFromDB();
+    }
+
+    private int nextKontonummer(){
+        int nextKontonummer = 0;
+        for(User u : userList){
+            while(nextKontonummer <= u.getKontonummer()){
+                nextKontonummer++;
+            }
+        }
+        return nextKontonummer;
+    }
+    
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
 }
